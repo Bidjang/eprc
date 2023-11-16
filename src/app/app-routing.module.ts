f3529abb676c0cf1a561b09b690a3de25b691e0c
Bidjang <bidjs@yahoo.fr>
2023-11-16 20:38:48 +0100

    modif

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'notfound',
+    component: NotfoundComponent
+  }, { 
+    path: '**',
+    redirectTo: 'notfound'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
